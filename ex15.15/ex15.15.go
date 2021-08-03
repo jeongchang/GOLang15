05132213200bee7a5385a25efc1b845ab52d0215
zepiro <marin223@naver.com>
2021-08-03 16:55:15 +0900

    chapter15 string commit

---
+++ ex15.15/ex15.15.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+	var str string = "Hello World"
+	var slice []byte = []byte(str)
+	/*
+		slice[2] = 'a'
+
+		fmt.Println(str)
+		fmt.Printf("%s\n", slice)
+	*/
+
+	stringHeader := (*reflect.StringHeader)(unsafe.Pointer(&str))
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&slice))
+
+	fmt.Printf("str :\t%x\n", stringHeader.Data)
+	fmt.Printf("slice:\t%x\n", sliceHeader.Data)
+}
---
+++ ex15.18/go.mod
@@ -0,0 +1,3 @@
+module goprojects/ex15.18
+
+go 1.16
